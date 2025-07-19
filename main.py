dd608bdef54efda6958ed64b51c3eae7a0269809
AndersonCarlosT <andercarlost@hotmail.com>
2025-07-19 12:20:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import io
-from datetime import datetime
+from datetime import datetime, timedelta
 import re
 
 st.set_page_config(page_title="Comparador de Perfiles", layout="wide")
